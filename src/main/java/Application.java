a3b187af0960ffec53ad42c1fae1a0fbbc0f6514
Elipongr <pelipongracz@gmail.com>
2020-06-16 00:49:16 +0200

    preapre to ask mirco tomorw

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,5 @@
 import Bot.BotListener;
-import Bot.BotService;
+import enums.Token;
 import help.PrivateConstReader;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
@@ -9,11 +9,11 @@ public class Application {
 
     public static void main(String[] args) {
         try {
-            final String  BOT_TOKEN = PrivateConstReader.getConstFromFile("C:\\projects\\tokens\\bot_token.txt");
+            final String  BOT_TOKEN = PrivateConstReader.getConstFromFile(Token.BOT_TOKEN.getToken());
             JDA jda = JDABuilder.createDefault(BOT_TOKEN).addEventListeners(new BotListener()).build();
             jda.awaitReady();
-            BotService botService = new BotService(jda);
-            botService.startGetMatchesService();
+//            BotService botService = new BotService(jda);
+//            botService.startGetMatchesService();
         } catch (Exception e) {
             e.printStackTrace();
         }
